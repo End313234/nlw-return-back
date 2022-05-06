502add238c6b41da458119bc626d44810678b08c
End313234 <end313234@protonmail.com>
2022-05-06 18:52:45 -0300

    Implement fastify instead ExpressJS

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-import express from "express";
+import Fastify from "fastify";
 
 import { Logger } from "./utils/logger";
 
 const PORT = 8080;
-const app = express();
+const app = Fastify();
 
 app.get("/", (_request, response) => {
-	response.send("Hello World!");
+	response.send("Hello World");
+});
+
+app.post("/feedbacks", (_request, response) => {
+	response.send("You're trying to create a feedback!");
 });
 
 app.listen(PORT, () => {
