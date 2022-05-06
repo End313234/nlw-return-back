51faa7a310d99509e95c397e0369e153e8eaea68
End313234 <end313234@protonmail.com>
2022-05-06 19:18:21 -0300

    Implement handler for empty body in json format

---
+++ src/enums/http-status.ts
@@ -0,0 +1,5 @@
+export enum HttpStatusEnum {
+	OK = 200,
+	CREATED = 201,
+	BAD_REQUEST = 400,
+}
--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,26 @@ import Fastify from "fastify";
 
 import { Logger } from "./utils/logger";
 
+import { HttpStatusEnum } from "./enums/http-status";
+
 const PORT = 8080;
 const app = Fastify();
+
+app.addHook("onRequest", (request, response, done) => {
+	if (!request.body && request.headers["content-type"] === "application/json") {
+		response.code(HttpStatusEnum.BAD_REQUEST).send({
+			message: "no body provided",
+		});
+	}
+	done();
+});
 
 app.get("/", (_request, response) => {
 	response.send("Hello World");
 });
 
-app.post("/feedbacks", (_request, response) => {
-	response.send("You're trying to create a feedback!");
+app.post("/feedbacks", (request, response) => {
+	response.send("You're trying to create a feedback");
 });
 
 app.listen(PORT, () => {
